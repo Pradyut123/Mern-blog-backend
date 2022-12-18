28f8b804355e55590cc826ca6561bddfe4df45b1
pradyut123 <pradyutsarma25@gmail.com>
2022-12-18 19:00:38 +0530

    backend done

---
+++ index.js
@@ -0,0 +1,40 @@
+const dotenv = require("dotenv");
+const express = require("express");
+const app = express();
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postsRoute = require("./routes/post");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
+const path = require("path");
+
+dotenv.config({ path: ".env" });
+require("./db/connection");
+
+app.use(express.json());
+app.use("/images", express.static(path.join(__dirname, "/images")));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/post", postsRoute);
+app.use("/api/categories", categoryRoute);
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`server started on ${PORT}`);
+});
